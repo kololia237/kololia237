5e3954b455e30394ec4d980d65aad0ba8e9b4826
Olha Kolisnyk <kol.olia237@gmail.com>
2021-11-22 12:15:56 +0200

    add all files

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')  # тут буде ссилка на головну сторінку сайту
+# @app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def posts_del(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "при видаленні статті відбулась помилка"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error"
+    else:
+        return render_template("post_update.html", article=article)
+
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error"
+    else:
+        return render_template("create-article.html")
+
+
+# @app.route('/user/<string:neme>/<int:id>')
+# def user():
+#     return "User page:" + neme + str(id)
+
+
+# запуск сервера для веб розробки
+if __name__ == '__main__':
+    # при запуску з'явиться ссилка на сайт на локал хості
+    app.run(debug=True)
